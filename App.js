8f0a28eb3c73178bfe134ce15733d8f68d8486c5
Hammad Ahmed <qwoo07072@gmail.com>
2024-03-23 06:43:02 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,69 @@
+//  Chapter 21(Changing Case)
+// part 1
+var allLower = userInput.toLowerCase();
+// part 2
+var x = "Hello World";
+x = x.toLowerCase();
+// part 3
+var y = "Hello World";
+y = y.toUpperCase();
+// part 4
+var originalString = "Hello World";
+var lowerCaseString = originalString.toLowerCase();
+// part 5
+var array = ["Hello", "World"];
+var newArrayElement = array[0].toLowerCase();
+// part 6
+var myString = "hello world";
+var upperCaseString = myString.toUpperCase();
+alert(upperCaseString);
+// part 7
+var cityName = "kaRacHi";
+var capitalizedCityName = cityName.charAt(0).toUpperCase() + cityName.slice(1).toLowerCase();
+console.log(capitalizedCityName);
+
+//   Chapter 22-25(String)
+// part 1
+var sameWords = "captain";
+var slicedWord = sameWords.slice(1, 3);
+console.log(slicedWord); 
+//part 2
+var sameWordl = "captain";
+var numberOfCharacters = sameWordl.length;
+console.log(numberOfCharacters);
+
+// part 3
+var animal = "elephant";
+var seg = animal.slice(1, 5);
+console.log(seg); 
+
+// part 4 , 5
+
+var myString = "Hello, World!";
+var numberOfCharacters = myString.length;
+console.log("Number of characters:", numberOfCharacters); 
+var slicedString = myString.slice(1, -3);
+console.log("Sliced string:", slicedString); 
+
+//part 6
+var text = "Let's go on a trip, and then go back home and relax.";
+var indx = text.lastIndexOf("go");
+console.log(indx); 
+
+//part 8
+var str = "abcde";
+var characterAtIndex2 = str.charAt(2);
+console.log(characterAtIndex2);
+
+// pat 11
+var text = "This is a sample text.";
+var cha = text.charAt(9);
+console.log(cha); 
+
+//part 12
+
+
+
+
+
+
